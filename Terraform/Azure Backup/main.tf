01592bd558f9cceefcb6e2d2b0f9e9534ca01820
Vladimir <vshvets@microsoft.com>
2024-05-10 19:29:23 -0400

    disclaimer

--- Terraform/Azure Backup/main.tf
+++ Terraform/Azure Backup/main.tf
@@ -4,7 +4,7 @@ and is not intended to be used in a production environment.  THIS
 SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED AS IS
 WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
 INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF
-MERCHANTABILITY ANDOR FITNESS FOR A PARTICULAR PURPOSE.  We
+MERCHANTABILITY AND OR FITNESS FOR A PARTICULAR PURPOSE.  We
 grant You a nonexclusive, royalty-free right to use and modify
 the Sample Code and to reproduce and distribute the object code
 form of the Sample Code, provided that You agree (i) to not use
