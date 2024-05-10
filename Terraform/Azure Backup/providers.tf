71c9b474451d6c948cb6500b9e7d69ec153784ab
Vladimir <vshvets@microsoft.com>
2024-05-10 19:24:51 -0400

    removing "prevent_deletion_if_contains_resources"

--- Terraform/Azure Backup/providers.tf
+++ Terraform/Azure Backup/providers.tf
@@ -6,11 +6,11 @@ terraform {
   }
 }
 
-provider "azurerm" {
+/* provider "azurerm" {
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
     }
 
   }
-}
+} */
